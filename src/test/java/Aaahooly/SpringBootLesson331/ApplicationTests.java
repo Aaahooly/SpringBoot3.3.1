7e23993cbfecda5f5b63f9faab30e2a4d71f4309
Aaahooly <andreygluh@yandex.ru>
2022-10-18 15:02:02 +0300

    SpringBoot of SpringCrud

---
+++ src/test/java/Aaahooly/SpringBootLesson331/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Aaahooly.SpringBootLesson331;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
